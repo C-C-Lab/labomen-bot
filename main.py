4cf6e549db4dc24cbdade3db142c51833c5682fa
niakachan <niaka@miraitasu.jp>
2021-03-13 23:23:32 +0900

    Update message

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 # 必要な設定を定義
 ACCESS_TOKEN = 'ODIwMjcwNTYwOTUyMTg4OTQ4.YEyufQ.XtPyfFRuytMTU06fW85jnVbsCVE'
 ready_message = "接続し、準備ができました"
+bot_ch_id = 813717329296228393
 
 random_contents = [
     "にゃーん",
@@ -28,8 +29,9 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == "鳴いて":
+    if message.content == "にあちゃん" and message.channel.id == bot_ch_id:
         content = random.choice(random_contents)
+        print('チャンネル名：' + str(message.channel))
         print('メッセージ受信を確認。 内容：' + message.content)
         await message.channel.send(content)
         print('反応を送信しました。 内容：' + content)
