64b1e3e2a0bebbc2b9c365f6f20d4e9552f9d781
niakachan <niaka@miraitasu.jp>
2021-03-13 22:40:51 +0900

    Add reply fuction

--- main.py
+++ main.py
@@ -5,17 +5,33 @@ import random
 ACCESS_TOKEN = 'ODIwMjcwNTYwOTUyMTg4OTQ4.YEyufQ.XtPyfFRuytMTU06fW85jnVbsCVE'
 ready_message = "接続し、準備ができました"
 
+random_contents = [
+    "にゃーん",
+    "わん！",
+    "コケッコッコー",
+    "お嬢",
+    "みら姉"
+]
+
 client = discord.Client()
 
+# アプリスタート時に走るイベント
 @client.event
 async def on_ready():
-    #Test Code
+    # 起動確認用バージョン情報
     print(ready_message)
-    print(discord.__version__)
+    print(discord.__title__ + " ライブラリのバージョン：" + discord.__version__)
+    print(discord.__copyright__)
 
+# メッセージ待受イベント
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
+    if message.content == "鳴いて":
+        content = random.choice(random_contents)
+        print('メッセージ受信を確認。 内容：' + message.content)
+        await message.channel.send(content)
+        print('反応を送信しました。 内容：' + content)
 
 client.run(ACCESS_TOKEN)
