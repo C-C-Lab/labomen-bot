f3b9890004e4e1b584ad2fb7cfeae61d03920755
niakachan <niaka@miraitasu.jp>
2021-03-14 18:17:36 +0900

    Update the definition of state transitions because the name was confusing.

--- janken.py
+++ janken.py
@@ -3,15 +3,28 @@ import discord
 import random
 import datetime
 
-states = ['OFF', 'ON', 'TIMEOUT']    #状態の定義
 
+# ここからtransitionsの設定
+# 状態の定義
+states = ['NORMAL_MODE','JANKEN_MODE', 'TIMEOUT']
+
+# 状態変化の定義
 transitions = [
-    {'trigger':'switch_ON', 'source':'OFF', 'dest':'ON'},       # 状態OFFからONへの遷移定義
-    {'trigger':'switch_OFF', 'source':'*', 'dest':'OFF'},      # 全状態からOFFへの遷移定義
-    {'trigger':'switch_TIMEOUT', 'source':'ON', 'dest':'TIMEOUT'},      # 状態ONからtimeoutへの遷移定義
+    {'trigger':'switch_NORMAL_MODE', 'source':'*', 'dest':'NORMAL_MODE'},
+    {'trigger':'switch_JANKEN_MODE', 'source':'*', 'dest':'JANKEN_MODE'},
+    {'trigger':'switch_TIMEOUT', 'source':'*', 'dest':'TIMEOUT'},
 ]
 
-# 必要な設定を定義
+class Model(object):
+    pass
+
+mode = Model()
+machine = Machine(model=mode, states=states, transitions=transitions, initial=states[0],
+                  auto_transitions=False, ordered_transitions=False)
+
+# ここまでtransitionsの設定
+
+# discordの設定
 ACCESS_TOKEN = 'ODIwMjcwNTYwOTUyMTg4OTQ4.YEyufQ.XtPyfFRuytMTU06fW85jnVbsCVE'
 ready_message = "接続し、準備ができました"
 # CC botチャンネル
@@ -60,22 +73,6 @@ janeken_favour_mes = [
 
 client = discord.Client()
 
-class Model(object):
-    pass
-
-model = Model()
-machine = Machine(model=model, states=states, transitions=transitions, initial=states[0],
-                  auto_transitions=False, ordered_transitions=False)
-
-# テストコード
-# print(model.state)
-# model.switch_ON()
-# print(model.state)
-# model.switch_TIMEOUT()
-# print(model.state)
-# model.switch_OFF()
-# print(model.state)
-
 # アプリスタート時に走るイベント
 @client.event
 async def on_ready():
@@ -92,7 +89,7 @@ async def on_message(message):
     if message.author.bot:
         return
     # じゃんけんモード
-    elif model.state == 'ON':
+    elif mode.state == 'JANKEN_MODE':
         print('時刻：' + dt_now)
         bot_hand = random.choice(janken_hand)
         if message.channel.id == bot_ch_id:
@@ -103,7 +100,7 @@ async def on_message(message):
                 result_mes = random.choice(janken_win_mes)
                 await message.channel.send(bot_hand + result_mes)
                 print('結果：botの勝ち　じゃんけんモード終了')
-                model.switch_OFF()
+                mode.switch_NORMAL_MODE()
             # bot敗北ルート
             elif message.content == "ぐー" and bot_hand == janken_hand_c \
                     or message.content == "ぱー" and bot_hand == janken_hand_g \
@@ -111,14 +108,14 @@ async def on_message(message):
                 result_mes = random.choice(janken_lose_mes)
                 await message.channel.send(bot_hand + result_mes)
                 print('結果：botの負け　じゃんけんモード終了')
-                model.switch_OFF()
+                mode.switch_NORMAL_MODE()
             # あいこルート
             elif message.content in bot_hand:
                 result_mes = random.choice(janeken_favour_mes)
                 await message.channel.send(bot_hand + result_mes)
                 print('結果：あいこ　じゃんけんモード継続')
     # 通常モード
-    elif model.state == 'OFF':
+    elif mode.state == 'NORMAL_MODE':
         print('時刻：' + dt_now)
         if message.content == "にあちゃん":
             print('チャンネル名：' + str(message.channel))
@@ -127,21 +124,14 @@ async def on_message(message):
             if message.channel.id == bot_ch_id:
                 content = random.choice(random_contents)
                 await message.channel.send(content)
-                print('IDが一致　反応：' + content)
+                print('channel idが一致 -> 反応：' + content)
             elif message.channel.id != bot_ch_id:
-                print('IDが不一致')
+                print('channel idが不一致 -> 反応なし')
         # じゃんけん起動
         elif message.channel.id == bot_ch_id and 'じゃんけん' in message.content:
-            model.switch_ON()
+            mode.switch_JANKEN_MODE()
             print('じゃんけんモードへ遷移')
             await message.channel.send(random.choice(janken_start_mes))
 
 
 client.run(ACCESS_TOKEN)
-
-
-# print(model.state)
-# model.switch_ON()
-# print(model.state)
-# model.switch_OFF()
-# print(model.state)
