cd1cff80a0fbabbd25f7dcd9c1e0f524cd4de25f
niakachan <niaka@miraitasu.jp>
2021-03-24 14:21:32 +0900

    Add counter for omikuji

--- modules/omikuji.py
+++ modules/omikuji.py
@@ -1,17 +1,20 @@
 import random
 import datetime
+from typing import Union
 from typing import Any
 
 from modules import utils
 from settings import omikuji_words
-from modules import slv
+from settings import init_user_states
+from datetime import date
 import asyncio
 
 words = omikuji_words
+today = datetime.date.today()
 
 
 # おみくじサイコロの重み設定 大吉・中吉・小吉・吉・凶
-omikuji_weights = [30, 25, 25, 20, 15]
+omikuji_weights = [27, 20, 20, 20, 10]
 
 
 async def play_omikuji(user_dict: dict, message: Any) -> dict:
@@ -25,14 +28,23 @@ async def play_omikuji(user_dict: dict, message: Any) -> dict:
     Returns:
         user_dict (dict): 更新済みuser_dict
     """
-    today = datetime.date.today()
-    last_date = slv.get_dict_value(user_dict, 'omikuji', 'date')
+    omikuji_dict: dict = user_dict.get('omikuji', init_user_states.INITIAL_STATES['omikuji'])
+    omikuji_count: int = omikuji_dict.get('count', 0)
+    daikichi_count: int = omikuji_dict.get('daikichi_count', 0)
+    kyo_count: int = omikuji_dict.get('kyo_count', 0)
+    attention_count: int = omikuji_dict.get('attention_count', 0)
+    last_date: Union[date, str] = omikuji_dict.get('date', '')
 
     if last_date != today:
         print('=========== おみくじを実行 ============')
-        results = list(words.OMIKUJI_RESULTS.keys())
-        result_list = random.choices(results, k=1, weights=omikuji_weights)
-        result = result_list[0]
+        # おみくじ結果の名称リストを取得（大吉～凶）
+        omikuji_results = list(words.OMIKUJI_RESULTS.keys())
+        # 設定したサイコロの重みをもとにおみくじ結果を算出
+        results = random.choices(omikuji_results, k=1, weights=omikuji_weights)
+        result = results[0]
+        omikuji_count += 1
+        daikichi_count += 1 if result == '大吉' else 0
+        kyo_count += 1 if result == '凶' else 0
         omikuji_mes = random.choice(words.OMIKUJI_RESULTS[result])
         head_message = random.choice(words.HEADER_MES)
         await utils.send_reply(message, head_message)
@@ -42,32 +54,19 @@ async def play_omikuji(user_dict: dict, message: Any) -> dict:
             print('結果：' + result)
             print(result_message)
             await utils.send_reply(message, result_message)
-            # slvへ日付と結果を記録
-            user_dict = update_omikuji_slv(user_dict, result_message)
+            omikuji_dict = {**omikuji_dict, **{'date': today, 'count': omikuji_count, 'daikichi_count': daikichi_count, 'kyo_count': kyo_count, 'result': result_message}}
+            user_dict = {**user_dict, **{'omikuji': omikuji_dict}}
     else:
         print('========= おみくじを実行不可 ==========')
+        attention_count += 1
         limit_mes = random.choice(words.LIMIT_MES)
         warning_mes = random.choice(words.WARNING_MES)
         repeat_intro_mes = random.choice(words.REPEAT_INTRO_MES)
-        last_result = slv.get_dict_value(user_dict, 'omikuji', 'result')
+        last_result = omikuji_dict.get('result', 'エラーです')
         bot_message = limit_mes + '\n' + warning_mes + \
             '\n' + repeat_intro_mes + last_result
         await utils.send_reply(message, bot_message)
         print(bot_message)
-    return user_dict
-
-
-def update_omikuji_slv(user_dict: dict, result: str) -> dict:
-    """おみくじの日付をslvに保存します。
-
-    Args:
-        user_dict (dict): user_slvから取り出したdict
-        result (str): 省略版の結果メッセージ
-
-    Returns:
-        user_dict (dict): 更新済みuser_dict
-    """
-    today = datetime.date.today()
-    updated_dict = {'date': today, 'result': result}
-    user_dict = slv.update_slv_dict(user_dict, 'omikuji', updated_dict)
+        omikuji_dict = {**omikuji_dict, **{'attention_count': attention_count}}
+        user_dict = {**user_dict, **{'omikuji': omikuji_dict}}
     return user_dict
